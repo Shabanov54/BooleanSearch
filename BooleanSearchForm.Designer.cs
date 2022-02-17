b36d02b3f97fc21a412f77c51ffc6a1d4ddd7d42
Станислав Шабанов <marazm56@gmail.com>
2022-02-17 12:47:33 +0700

    Добавьте файлы проекта.

---
+++ BooleanSearchForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace BooleanSearch
+{
+    partial class BooleanSearchForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BooleanSearchForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(10, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(340, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "linkedin.com",
+            "fb.com",
+            "Хабр карьера",
+            "vk.com",
+            "github.com"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(179, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Должность (Вводи через запятую)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(10, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(340, 26);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Город (Вводи через запятую)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(503, 17);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Поиск по телеграму";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(503, 58);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(106, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Поиск по Почте";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(15, 226);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(772, 212);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox4_MouseClick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Сформировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Доп. Параметры (Вводи через запятую)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(10, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(340, 26);
+            this.textBox3.TabIndex = 6;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(503, 99);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(152, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Открыт к предложениям";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // BooleanSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BooleanSearchForm";
+            this.Text = "Boolean Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ BooleanSearchForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooleanSearch
+{
+    public partial class BooleanSearchForm : Form
+    {
+        string deletedValue = "-inurl:dir -vacancies -companies -courses -resumes?";
+        string habr = "site:moikrug.ru OR site:career.habr.com";
+        string link = "site:linkedin.com/in OR site:linkedin.com/pub";
+        string vk = "site:vk.com";
+        string github = "site:github.com";
+        string fb = "site:fb.com";
+        public BooleanSearchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> positionList = new List<string>();
+            List<string> extraVAlList = new List<string>();
+            List<string> cityList = new List<string>();
+            string telegram = "";
+            string mail = "";
+            string open = "";
+            string citystring = "";
+            string etraVAl = "";
+            string stringPosition = "";
+
+
+            while (true)
+            {
+                var selectItemComboboxIndex = comboBox1.SelectedIndex;
+                string selectItemCombobox = "";
+                if (selectItemComboboxIndex == 0)
+                {
+                    selectItemCombobox = link;
+                }
+                if (selectItemComboboxIndex == 1)
+                {
+                    selectItemCombobox = fb;
+                }
+                if (selectItemComboboxIndex == 2)
+                {
+                    selectItemCombobox = habr;
+                }
+                if (selectItemComboboxIndex == 3)
+                {
+                    selectItemCombobox = vk;
+                }
+                if (selectItemComboboxIndex == 4)
+                {
+                    selectItemCombobox = github;
+                }
+                if (comboBox1.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Забыли выбрать рессурс");
+                    break;
+                }
+                if (textBox1.Text != "")
+                {
+                    var splitposition = textBox1.Text.Split(new Char[] { ',' });
+                    foreach (var splited in splitposition)
+                    {
+                        positionList.Add($"\"{splited}\"");
+                    }
+                    stringPosition = string.Join(" ", positionList.ToList());
+                }
+                if (textBox3.Text != "")
+                {
+                    var spletExtraVAl = textBox3.Text.Split(new Char[] { ',' });
+                    foreach (var splited in spletExtraVAl)
+                    {
+                        extraVAlList.Add($"\"{splited}\"");
+                    }
+                    etraVAl = string.Join(" ", extraVAlList.ToList());
+                }
+                if (textBox2.Text!="")
+                {
+                    var splitedCity = textBox2.Text.Split(new Char[] { ',' });
+                    foreach (var splited in splitedCity)
+                    {
+                        cityList.Add($"\"{splited}\"");
+                    }
+                    citystring = string.Join(" ", cityList.ToList());
+                }
+
+
+                if (checkBox1.Checked == true)
+                {
+                    telegram = "\"Telegram: @\"";
+                }
+                if (checkBox2.Checked == true)
+                {
+                    mail = "(\"@gmail.com\" OR \"@mail.ru\" OR \"@yandex.ru\" OR \"@outlook.com\" OR \"@rambler.ru \")";
+                }
+                if (checkBox3.Checked == true)
+                {
+                    open = "\"открыт к предложениям\"";
+                }
+
+                textBox4.Text = $"{selectItemCombobox} {deletedValue} {etraVAl} {stringPosition} {citystring} {telegram} {mail} {open}";
+
+                break;
+            }
+
+        }
+
+        private void textBox4_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Clipboard.SetText(textBox4.Text);
+            }
+        }
+    }
+}
